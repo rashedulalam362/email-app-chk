7cadcec3aef9571ce048f4495c210d5eea2469f4
unknown <rashedul.alam362@gmail.com>
2023-09-08 22:52:43 +0600

    process

---
+++ app/api/Token/route.js
@@ -0,0 +1,33 @@
+import { jwtVerify,SignJWT } from "jose";
+import { NextResponse } from 'next/server';
+export async function GET(req, res) {
+
+const key=new TextEncoder().encode(process.env.JWT_KEY)
+const payload={email:'rashed@gmail.com', password:'ab123'}
+
+const token=await new SignJWT(payload)
+          .setProtectedHeader({alg:'HS256'})
+          .setIssuedAt()
+          .setIssuer('http://localhost:3000/')
+          .setExpirationTime('2h')
+          .sign(key)
+
+       return NextResponse.json({token: token})   
+
+}
+
+
+
+export async function POST(req,res){
+
+    const jsonBody=await req.json()
+    const token=jsonBody['token']
+    const key=new TextEncoder().encode(process.env.JWT_KEY)
+
+    const decoded=await jwtVerify(token,key)
+
+
+    return NextResponse.json(decoded)
+
+
+}
--- app/api/email/route.js
+++ app/api/email/route.js
@@ -1,42 +1,48 @@
 import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
-export async function GET(req, res) {
-  const {searchParams} =new URL(req.url)
-  toEmail= searchParams.get('email')
+
+export async function GET(req,res){
+
+  const { searchParams } = new URL(req.url);
+  const toEmail = searchParams.get('email');
+
+  // Create a transporter
+
+  const Transporter=nodemailer.createTransport({
+    host: "mail.teamrabbil.com",
+    port:25,
+    secure: false,
+    auth:{
+      user:"info@teamrabbil.com",
+      pass: '~sR4[bhaC[Qs'
+    },
+    tls: { rejectUnauthorized: false },
+
+  })
+        // Prepare the email
+    let myEmail = {
+      from: 'text email <info@teamrabbil.com>',
+      to: toEmail,
+      subject: 'test mail',
+      text: 'test mail'
+    }
+
+  // Send the email
+
+  try {
+    await Transporter.sendMail(myEmail);
+   
+   return NextResponse.json({msg:"success"})
+
+
+  } 
+  catch (error) {
+    return NextResponse.json({msg:"failed"})
+  }
+    
+
+
 
 }
-//transporter 
 
-let transporter=nodemailer.createTransport({
-  host:'mail.teamrabbil.com',
-  port:25,
-  secure:false,
-  auth:{
-    user:'info@teamrabbil.com',
-    pass:'~sR4[bhaC[Qs'
-  },
-  tls:{rejectUnauthorized:false}
-})
-
-// preapre-email
-
-let myEmail ={
-    from:'text email <info@teamrabbil.com>',
-    to:toEmail,
-    subject:'test mail',
-    text:'test mail'
-}
-
-// try {
-//  await sendMail(myEmail)
- 
-//  return NextResponse.json({mes:'success'})
-    
-// } catch (error) {
-
-//     return NextResponse.json({mes:'fail'})
-// }
-    
-
-result=await transporter.sendMail(myEmail)
-console.log(result);
